0ae39d9a6978f7a53a34328bd9e8964a80082ad7
lidiia <lidiia@LIDA>
2024-04-05 10:47:11 +0300

    Добавьте файлы проекта.

---
+++ laba1EF/Window2.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace laba1EF
+{
+    /// <summary>
+    /// Логика взаимодействия для Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        private dartbaseEntities content = new dartbaseEntities();
+        public Window2()
+        {
+            InitializeComponent();
+            MyDataGrid.ItemsSource = content.Platforms.ToList();
+        }
+
+        private void DevelopersButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow DevelopersDS = new MainWindow();
+            DevelopersDS.Show();
+            Close();
+        }
+
+        private void GamesButton_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 GamesDS = new Window1();
+            GamesDS.Show();
+            Close();
+        }
+
+        private void PlatformsButton_Click(object sender, RoutedEventArgs e)
+        {
+            Window2 PlatformsDS = new Window2();
+            PlatformsDS.Show();
+            Close();
+        }
+    }
+}
